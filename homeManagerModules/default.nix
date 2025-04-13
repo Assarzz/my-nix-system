00dcb18499f29a074c7bd57c1cc84fcae60411b7
Assarzz <assarlannerbornzz@gmail.com>
2025-04-13 08:26:21 +0200

    update

--- homeManagerModules/default.nix
+++ homeManagerModules/default.nix
@@ -1,4 +1,6 @@
-{pkgs, ...}:
+{pkgs, lib,  ...}:let 
+ssh-keyname = "id_ed25519";
+in
 {
   xdg.userDirs.enable = true;
   xdg.userDirs.createDirectories = true;
@@ -8,6 +10,18 @@
     username = "assar";
     homeDirectory = "/home/assar";
     stateVersion = "25.05";
+
+    activation = {
+      generate-ssh-keys = lib.hm.dag.entryAfter [ "writeBoundary" "installPackages" ] ''
+        if [ ! -f "$HOME/.ssh/${ssh-keyname}" ]; then
+          mkdir -p $HOME/.ssh
+          ${pkgs.openssh}/bin/ssh-keygen -f $HOME/.ssh/${ssh-keyname} -N ""
+        fi
+        '';
+      # generate-ssh-keys= lib.hm.dag.entryAfter ["myActivationAction1"] ''
+      # <command1>
+      # '';
+    };
   };
 
   programs = {
@@ -19,7 +33,7 @@
     };
     ssh = {
       enable = true;
-      
+      #addKeysToAgent
     };
   };
 }
