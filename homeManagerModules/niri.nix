f03eff3617878cf304d28f6d721a6e5d3b7ef9e9
Assarzz <assarlannerbornzz@gmail.com>
2025-04-24 21:16:38 +0200

    update

--- homeManagerModules/niri.nix
+++ homeManagerModules/niri.nix
@@ -51,9 +51,9 @@
             "Mod+Shift+R".action = switch-preset-window-height;
 
 
-            "Print".action = screenshot = {write-to-disk = true;};
-            "Ctrl+Print".action = screenshot-screen = {write-to-disk = true;};
-            "Alt+Print".action = screenshot-window = {write-to-disk = true;};
+            "Print".action = screenshot; # {write-to-disk = true;};
+            #"Ctrl+Print".action.screenshot-screen = {write-to-disk = true;};
+            #"Alt+Print".action.screenshot-window = {write-to-disk = true;};
 
             # Powers off the monitors. To turn them back on, do any input like
             # moving the mouse or pressing any other key.
