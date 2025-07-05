ade118b3cf7f30da1e1bfa023b62aa4ba8943376
Assarzz <assarlannerbornzz@gmail.com>
2025-07-05 21:42:11 +0200

    forgot quotation around some dev rule values

--- nas/hdparam.mod.nix
+++ nas/hdparam.mod.nix
@@ -11,13 +11,13 @@
           # nas
           (mkRule [
             ''ACTION=="add|change"''
-            ''ENV{ID_WWN}==0x5000039dc8cb3799''
+            ''ENV{ID_WWN}=="0x5000039dc8cb3799"''
             ''RUN+="${pkgs.hdparm}/bin/hdparm -B 90 $devnode"''
           ])
           # backup
           (mkRule [
             ''ACTION=="add|change"''
-            ''ENV{ID_WWN}==0x5000039dc8cb1e82''
+            ''ENV{ID_WWN}=="0x5000039dc8cb1e82"''
             ''RUN+="${pkgs.hdparm}/bin/hdparm -Y $devnode"''
           ])
         ]);
