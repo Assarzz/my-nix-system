b1ab2972bd4bdfd408e7dd11ba796d96b838daab
Assarzz <assarlannerbornzz@gmail.com>
2025-07-25 13:06:13 +0200

    update

--- nas/docker.mod.nix
+++ nas/docker.mod.nix
@@ -3,12 +3,16 @@
 
     {
       virtualisation.docker = {
-        # Disabling the system wide Docker daemon.
-        enable = false;
-
-        rootless = {
-          enable = true;
-          setSocketVariable = true;
+        enable = true;
+        # Set up resource limits
+        daemon.settings = {
+          experimental = true;
+          default-address-pools = [
+            {
+              base = "172.30.0.0/16";
+              size = 24;
+            }
+          ];
         };
       };
 
