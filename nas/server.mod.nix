ab9a5acf8b04b75ca2567fd918c7209339cc6e50
Assarzz <assarlannerbornzz@gmail.com>
2025-08-28 15:15:40 +0200

    update

--- nas/server.mod.nix
+++ nas/server.mod.nix
@@ -1,7 +1,6 @@
 /*
-No Imperative steps needed.
-You can reference data directories from the different services in the backup mod file if you want to back them up. This is not done automatically.
-
+  No Imperative steps needed.
+  You can reference data directories from the different services in the backup mod file if you want to back them up. This is not done automatically.
 */
 let
 
@@ -17,7 +16,10 @@ let
     "forgejo.an" = "8082";
     "qbittorrent.an" = "8080";
   };
-  excludeFromAutoGen = ["forgejo.an" "qbittorrent.an" ];
+  excludeFromAutoGen = [
+    "forgejo.an"
+    "qbittorrent.an"
+  ];
   # dnsmasq option format : -A, --address=/<domain>[/<domain>...]/[<ipaddr>]
   dns_addresses =
     "/" + builtins.concatStringsSep "/" ((builtins.attrNames dns_domains) ++ [ serverIP ]);
@@ -50,39 +52,42 @@ in
     }
 
     # nginx server
-    ({lib, ...}:{
-      # A benefit of using a reverse proxy is that i only need to is expose these ports on the firewall for all server services using nginx.
-      networking.firewall.allowedTCPPorts = [
-        80
-        443
-      ];
+    (
+      { lib, ... }:
+      {
+        # A benefit of using a reverse proxy is that i only need to is expose these ports on the firewall for all server services using nginx.
+        networking.firewall.allowedTCPPorts = [
+          80
+          443
+        ];
 
-      services.nginx = {
-        enable = true;
-        recommendedProxySettings = true;
-        recommendedTlsSettings = true;
+        services.nginx = {
+          enable = true;
+          recommendedProxySettings = true;
+          recommendedTlsSettings = true;
 
-        # virtual hosts that dont require any custom configuration.
-        # Explanation for how nginx know what request to send to which port : so even though all the domains get turned into the same ip, after the browser has received the ip from the dns server of the specified domain it THEN creates its finished request to the nginx server in which the originally specified domain is included as the Host header, which presumably nginx maps to virtualHosts.
-        virtualHosts = (
-          builtins.mapAttrs (_: port: {
-            enableACME = false;
-            forceSSL = false;
-            locations."/" = {
-              # If you include extra after the domain name, you can add extra functionality. "/" is a catch all.
-              proxyPass = "http://127.0.0.1:${port}";
-              proxyWebsockets = true; # needed if you need to use WebSocket
-              extraConfig =
-                # required when the target is also TLS server with multiple hosts
-                "proxy_ssl_server_name on;"
-                +
-                  # required when the server wants to use HTTP Authentication
-                  "proxy_pass_header Authorization;";
-            };
-          }) lib.filterAttrs (name : _ : !builtins.elem name excludeFromAutoGen ) dns_domains
-        );
-      };
-    })
+          # virtual hosts that dont require any custom configuration.
+          # Explanation for how nginx know what request to send to which port : so even though all the domains get turned into the same ip, after the browser has received the ip from the dns server of the specified domain it THEN creates its finished request to the nginx server in which the originally specified domain is included as the Host header, which presumably nginx maps to virtualHosts.
+          virtualHosts = (
+            builtins.mapAttrs (_: port: {
+              enableACME = false;
+              forceSSL = false;
+              locations."/" = {
+                # If you include extra after the domain name, you can add extra functionality. "/" is a catch all.
+                proxyPass = "http://127.0.0.1:${port}";
+                proxyWebsockets = true; # needed if you need to use WebSocket
+                extraConfig =
+                  # required when the target is also TLS server with multiple hosts
+                  "proxy_ssl_server_name on;"
+                  +
+                    # required when the server wants to use HTTP Authentication
+                    "proxy_pass_header Authorization;";
+              };
+            }) lib.filterAttrs (name: _: !builtins.elem name excludeFromAutoGen) dns_domains
+          );
+        };
+      }
+    )
 
     # jellyfin server
     {
@@ -94,12 +99,21 @@ in
     }
     # kavita reader server
     (
-      { lib, ... }:
+      { lib, pkgs, ... }:
+      let
+        # pkgs.runCommand : https://ryantm.github.io/nixpkgs/builders/trivial-builders/
+        # The result is a path in the /nix/store, e.g., /nix/store/....-kavita-token-key
+        kavitaTokenFile = pkgs.runCommand "kavita-token-key" { } ''
+          head -c 64 /dev/urandom | base64 --wrap=0 > $out
+        '';
+      in
       {
         services.kavita = {
           enable = true;
           dataDir = "${servicesDataDir}/share/kavita";
           settings.Port = lib.toInt dns_domains."kavita.an";
+          user = "assar";
+          tokenKeyFile = kavitaTokenFile;
         };
 
       }
@@ -150,66 +164,66 @@ in
     )
 
     {
-        services.nginx.virtualHosts."qbittorrent.an" = {
-          enableACME = false;
-          forceSSL = false;
-          locations."/" = {
-            proxyPass = "http://192.168.100.11:${dns_domains."qbittorrent.an"}";
-            proxyWebsockets = true; # needed if you need to use WebSocket
-            extraConfig =
-              # required when the target is also TLS server with multiple hosts
-              "proxy_ssl_server_name on;"
-              +
-                # required when the server wants to use HTTP Authentication
-                "proxy_pass_header Authorization;";
-          };
+      services.nginx.virtualHosts."qbittorrent.an" = {
+        enableACME = false;
+        forceSSL = false;
+        locations."/" = {
+          proxyPass = "http://192.168.100.11:${dns_domains."qbittorrent.an"}";
+          proxyWebsockets = true; # needed if you need to use WebSocket
+          extraConfig =
+            # required when the target is also TLS server with multiple hosts
+            "proxy_ssl_server_name on;"
+            +
+              # required when the server wants to use HTTP Authentication
+              "proxy_pass_header Authorization;";
         };
-        networking.nat = {
-          enable = true;
-          internalInterfaces = [ "ve-+" ]; # This is a wildcard that tells the NAT system: "Watch for traffic coming from any interface that starts with ve-."
-          externalInterface = "ens3";
-          # Lazy IPv6 connectivity for the container
-          enableIPv6 = true;
-        };
+      };
+      networking.nat = {
+        enable = true;
+        internalInterfaces = [ "ve-+" ]; # This is a wildcard that tells the NAT system: "Watch for traffic coming from any interface that starts with ve-."
+        externalInterface = "ens3";
+        # Lazy IPv6 connectivity for the container
+        enableIPv6 = true;
+      };
 
-        containers.qbittorrent = {
-          autoStart = true; # Starts the container automatically when the host boots up.
-          privateNetwork = true; # Creates a separate network namespace for the container, ensuring network isolation.
+      containers.qbittorrent = {
+        autoStart = true; # Starts the container automatically when the host boots up.
+        privateNetwork = true; # Creates a separate network namespace for the container, ensuring network isolation.
 
-          # ip address of the virtual
-          hostAddress = "192.168.100.10";
-          localAddress = "192.168.100.11";
-          hostAddress6 = "fc00::1";
-          localAddress6 = "fc00::2";
-          config =
-            {
-              lib,
-              ...
-            }:
-            {
+        # ip address of the virtual
+        hostAddress = "192.168.100.10";
+        localAddress = "192.168.100.11";
+        hostAddress6 = "fc00::1";
+        localAddress6 = "fc00::2";
+        config =
+          {
+            lib,
+            ...
+          }:
+          {
 
-              services.qbittorrent = {
-                enable = true;
-                webuiPort = lib.toInt dns_domains."qbittorrent.an";
-              };
-              networking = {
-                # This inner container must be accessible from the outside.
-                firewall.allowedTCPPorts = [ (lib.toInt dns_domains."qbittorrent.an") ];
-
-                # Use systemd-resolved inside the container
-                # Workaround for bug https://github.com/NixOS/nixpkgs/issues/162686
-                # "Gemini: This is an important networking detail. It tells the container not to simply copy the host's DNS settings.
-                # Instead, it runs its own DNS resolver (systemd-resolved) inside the container. This improves isolation and prevents certain network-related bugs."
-                useHostResolvConf = lib.mkForce false;
-              };
-
-              services.resolved.enable = true;
-
-              system.stateVersion = "25.05";
-
+            services.qbittorrent = {
+              enable = true;
+              webuiPort = lib.toInt dns_domains."qbittorrent.an";
             };
-        };
-      }
+            networking = {
+              # This inner container must be accessible from the outside.
+              firewall.allowedTCPPorts = [ (lib.toInt dns_domains."qbittorrent.an") ];
+
+              # Use systemd-resolved inside the container
+              # Workaround for bug https://github.com/NixOS/nixpkgs/issues/162686
+              # "Gemini: This is an important networking detail. It tells the container not to simply copy the host's DNS settings.
+              # Instead, it runs its own DNS resolver (systemd-resolved) inside the container. This improves isolation and prevents certain network-related bugs."
+              useHostResolvConf = lib.mkForce false;
+            };
+
+            services.resolved.enable = true;
+
+            system.stateVersion = "25.05";
+
+          };
+      };
+    }
 
   ];
 }
