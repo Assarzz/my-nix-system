360c5f09b3d40ea095477a5f037c84f5c7dc2b33
Assarzz <assarlannerbornzz@gmail.com>
2025-08-29 15:47:04 +0200

    update

--- users.mod.nix
+++ users.mod.nix
@@ -16,6 +16,7 @@
         hashedPassword = "$y$j9T$mxvQmMdU/WfPfvnh4f1xk1$g0on.Tq4lJWy43YN9ok/GQ6bufbeG45NCax3HrI1sa6";
         openssh.authorizedKeys.keys = [ "ssh-dss AAAAB3Nza... assar@foobar" ]; # dont know what this does. TODO test removing it.
       };
+      users.groups."assar" = {};
       users.users.root = {
         hashedPassword = "$y$j9T$mxvQmMdU/WfPfvnh4f1xk1$g0on.Tq4lJWy43YN9ok/GQ6bufbeG45NCax3HrI1sa6";
       };
