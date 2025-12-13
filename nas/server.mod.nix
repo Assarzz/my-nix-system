4bf7e4832667809cf5ef710de6c731a628a9854d
Assar <assarlannerbornzz@gmail.com>
2025-12-13 11:07:46 +0100

    minecraft server configuration change.

--- nas/server.mod.nix
+++ nas/server.mod.nix
@@ -244,11 +244,50 @@ in
  ({pkgs, ... }:
 
 {
-  # 1. Enable the Minecraft Server
-  services.minecraft-server = {
+services.minecraft-server = {
     enable = true;
-    eula = true; # You must accept the EULA
-    openFirewall = true; 
+    eula = true;
+    openFirewall = true;
+
+    # 1. Switch to Paper for better performance
+    package = pkgs.papermc; 
+
+    # 2. Optimize View Distance settings
+    serverProperties = {
+      # "View Distance": How far you can SEE (Chunks are loaded visually)
+      # 10 is standard, 12-16 is "far". (Start with 12)
+      view-distance = 16;
+
+      # "Simulation Distance": How far physics run (Crops grow, mobs move)
+      # Keep this low (5-6) to save CPU on your NAS while keeping nice views.
+      simulation-distance = 6;
+      
+      # Other useful tweaks
+      motd = "My High Performance Server";
+      max-players = 10;
+    };
+    
+jvmOpts = builtins.concatStringsSep " " [
+      "-Xms8G -Xmx8G"
+      "-XX:+UseG1GC"
+      "-XX:+ParallelRefProcEnabled"
+      "-XX:MaxGCPauseMillis=200"
+      "-XX:+UnlockExperimentalVMOptions"
+      "-XX:+DisableExplicitGC"
+      "-XX:+AlwaysPreTouch"
+      "-XX:G1NewSizePercent=30"
+      "-XX:G1MaxNewSizePercent=40"
+      "-XX:G1HeapRegionSize=8M"
+      "-XX:G1ReservePercent=20"
+      "-XX:G1HeapWastePercent=5"
+      "-XX:G1MixedGCCountTarget=4"
+      "-XX:InitiatingHeapOccupancyPercent=15"
+      "-XX:G1MixedGCLiveThresholdPercent=90"
+      "-XX:G1RSetUpdatingPauseTimePercent=5"
+      "-XX:SurvivorRatio=32"
+      "-XX:+PerfDisableSharedMem"
+      "-XX:MaxTenuringThreshold=1"
+    ];
   };
 
   services.tailscale.enable = true;
